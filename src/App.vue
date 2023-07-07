428fb465885e7b1ecadf684a07733a36552cc6f0
LS-plan <2016040219@s.upc.edu.cn>
2023-07-07 15:43:14 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <todolist></todolist>
+  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todolist from './components/todolist';
 
 export default {
-  name: 'App',
+  data() {
+    return {
+
+    }
+  },
   components: {
-    HelloWorld
+    todolist
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
